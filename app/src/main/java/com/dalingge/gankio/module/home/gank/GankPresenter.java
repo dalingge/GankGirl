047191e78aef61923abf551d42565003b303451a
dalingge <445850053@qq.com>
2017-05-12 18:02:07 +0800

    优化

--- app/src/main/java/com/dalingge/gankio/module/home/gank/GankPresenter.java
+++ app/src/main/java/com/dalingge/gankio/module/home/gank/GankPresenter.java
@@ -3,17 +3,9 @@ package com.dalingge.gankio.module.home.gank;
 import android.os.Bundle;
 
 import com.dalingge.gankio.common.base.BaseRxPresenter;
-import com.dalingge.gankio.common.rxjava.Function0;
-import com.dalingge.gankio.data.model.GankBean;
-import com.dalingge.gankio.network.HttpExceptionHandle;
 import com.dalingge.gankio.network.HttpRetrofit;
 import com.dalingge.gankio.network.RequestCommand;
 import com.dalingge.gankio.network.RetryWhenNetworkException;
-
-import java.util.List;
-
-import io.reactivex.Observable;
-import io.reactivex.functions.BiConsumer;
 
 /**
  * Created by dingboyang on 2016/11/12.
@@ -26,23 +18,10 @@ public class GankPresenter extends BaseRxPresenter<GankFragment> {
         super.onCreate(savedState);
 
         restartableFirst(RequestCommand.REQUEST_HOME_GANK,
-                new Function0<Observable<List<GankBean>>>() {
-                    @Override
-                    public Observable<List<GankBean>> apply() {
-                        return HttpRetrofit.getInstance().apiService.getData(requestContext.getType(), requestContext.getPage())
-                                .compose(HttpRetrofit.toTransformer())
-                                .retryWhen(new RetryWhenNetworkException());
-                    }
-                }, new BiConsumer<GankFragment, List<GankBean>>() {
-                    @Override
-                    public void accept(GankFragment gankFragment, List<GankBean> gankBeanList) throws Exception {
-                        gankFragment.onAddData(gankBeanList);
-                    }
-                }, new BiConsumer<GankFragment, HttpExceptionHandle.ResponeThrowable>() {
-                    @Override
-                    public void accept(GankFragment gankFragment, HttpExceptionHandle.ResponeThrowable responeThrowable) throws Exception {
-                        gankFragment.onNetworkError(responeThrowable);
-                    }
-                });
+                () -> HttpRetrofit.getInstance().apiService.getData(requestContext.getType(), requestContext.getPage())
+                        .compose(HttpRetrofit.toTransformer())
+                        .retryWhen(new RetryWhenNetworkException()),
+                (gankFragment, gankBeanList) -> gankFragment.onAddData(gankBeanList),
+                (gankFragment, responeThrowable) -> gankFragment.onNetworkError(responeThrowable));
     }
 }
